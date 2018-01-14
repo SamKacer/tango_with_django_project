e2927499a13efe9b2782e2db84252ef1209ab06d
Samuel Kacer <2093358k@student.gla.ac.uk>
2018-01-14 14:51:21 +0000

    added about page with basic message

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Howdy yall! This is from Rango.")
+
+def about(request):
+    return HttpResponse('Rango says here is the about page.')
