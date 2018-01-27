5fae399f5d876634d5bab6bef3d0832673c2f3d6
Samuel Kacer <2093358k@student.gla.ac.uk>
2018-01-27 10:11:04 +0000

    finished chapter 7, including exercises

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^category/(?P<category_name_url>[\w\-]+)/$', views.show_category, name ='show_category'), 
     url(r'^index/$', views.index, name = 'index'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name="add_page"),
    ]
